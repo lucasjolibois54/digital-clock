0c97a303ea9d80ba79fd80aa29e0d71797d07e88
lucasjolibois54 <lucasjolibois@icloud.com>
2023-02-16 22:28:41 +0000

    Update TimerTwo.js

--- src/components/TimerTwo.js
+++ src/components/TimerTwo.js
@@ -1,30 +1,26 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
 
 export default class TimerTwo extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {date: new Date().toLocaleTimeString()};
-      }
-    
-      componentDidMount() {
-        this.timerID = setInterval(
-          () => this.tick(),
-          1000
-        );
-      }
-    
-      componentWillUnmount() {
-        clearInterval(this.timerID);
-      }
-    
-      tick() {
-        this.setState({
-          date: new Date().toLocaleTimeString()
-        });
-      }
-    
-    /*state={
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date().toLocaleTimeString() };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date().toLocaleTimeString(),
+    });
+  }
+
+  /*state={
         time : new Date().toLocaleTimeString("en-US"),
       }
 
@@ -39,15 +35,30 @@ export default class TimerTwo extends Component {
       componentWillUnmount() {
         clearInterval(this.timerID);
       }*/
-      
-      render(){
-        return (
-          <div>
-            <p>Current Time : {this.state.date}</p>
-          </div>
-        );
-      }
-    }
 
-
-
+  render() {
+    return (
+      <div>
+        <p className="bottom-5 absolute mb-2 right-5 developer mt-10 italic font-normal mr-10">
+          Developed by{" "}
+          <a
+            className="underline text-gray-500 italic font-normal"
+            href="https://www.lucasjolibois.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Lucas Jolibois
+          </a>
+        </p>
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+          <p className="text-5xl sm:text-7xl font-semibold text-gray-200">
+            {this.state.date}
+          </p>
+          <p className=" text-gray-300 max-w-sm">
+            <b>Made using:</b> Constructor, ComponentDidMount, ComponentDidUnmount, render
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
