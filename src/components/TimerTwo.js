bde303e8b1a2e59f7d4177071b09f2d7384504af
lucasjolibois54 <lucasjolibois@icloud.com>
2023-02-16 22:42:09 +0000

    Update TimerTwo.js

--- src/components/TimerTwo.js
+++ src/components/TimerTwo.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link, Outlet } from "react-router-dom";
 
 export default class TimerTwo extends Component {
   constructor(props) {
@@ -40,24 +41,22 @@ export default class TimerTwo extends Component {
     return (
       <div>
         <p className="bottom-5 absolute mb-2 right-5 developer mt-10 italic font-normal mr-10">
-          Developed by{" "}
-          <a
-            className="underline text-gray-500 italic font-normal"
-            href="https://www.lucasjolibois.com/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Lucas Jolibois
-          </a>
+          Visit the:{" "}
+          <Link to="/" className="underline text-gray-500 italic font-normal">
+            Functional Component version
+          </Link>
         </p>
+
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
           <p className="text-5xl sm:text-7xl font-semibold text-gray-200">
             {this.state.date}
           </p>
           <p className=" text-gray-300 max-w-sm">
-            <b>Made using:</b> Constructor, ComponentDidMount, ComponentDidUnmount, render
+            <b>Made using:</b> Constructor, ComponentDidMount,
+            ComponentDidUnmount, render
           </p>
         </div>
+        <Outlet />
       </div>
     );
   }
