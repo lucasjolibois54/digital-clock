a8ca14a843feecd8281018b776b02a6a858e3063
lucasjolibois54 <lucasjolibois@icloud.com>
2023-02-16 19:01:28 +0000

    Update TimerTwo.js

--- src/components/TimerTwo.js
+++ src/components/TimerTwo.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
 
+
 export default class TimerTwo extends Component {
-  render() {
-    return (
-      <div>TimerTwo</div>
-    )
-  }
-}
+    
+    state={
+        time : new Date(),
+      }
+
+
+      componentDidMount() {
+        this.interval = setInterval(() => Date(), 1000);
+      }
+    
+      componentWillUnmount() {
+        clearInterval(this.interval);
+      }
+      
+      render(){
+        return (
+          <div className="App">
+            <p>Current Time : {this.state.time.toLocaleTimeString("en-US")}</p>
+          </div>
+        );
+      }
+    }
