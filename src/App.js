d3301b596d37e10188b4f455e62fd344437da374
lucasjolibois54 <lucasjolibois@icloud.com>
2023-02-16 19:01:24 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {Routes, Route} from 'react-router-dom'
 //import Nav from './components/Nav';
 import Home from './pages/Home';
+import SecondTimer from './pages/SecondTimer';
 import './App.css';
 
 /*npm i react-router-dom*/
@@ -11,6 +12,7 @@ function App() {
     <Routes>
         {/* <Route path="/" element={<Nav/>}> */}
         <Route path="/" index element={<Home/>}/>
+        <Route path="/second-timer" element={<SecondTimer/>}/>
         {/* <Route path="/use-effect-basics" element={<BasicsOfUseEffects/>}/> */}
         {/* </Route> */}
     </Routes>
