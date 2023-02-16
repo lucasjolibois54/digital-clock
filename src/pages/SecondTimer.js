031f6e670bb76b5dd084d78a76f4c081e42392e7
lucasjolibois54 <lucasjolibois@icloud.com>
2023-02-16 19:01:27 +0000

    Create SecondTimer.js

---
+++ src/pages/SecondTimer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TimerTwo from '../components/TimerTwo'
+
+function SecondTimer() {
+  return (
+    <div>
+        <TimerTwo/>
+    </div>
+  )
+}
+
+export default SecondTimer
